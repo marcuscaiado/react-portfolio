b4aa379ada26ae54d834fe7ab9c1f0434a9ed6dc
marcuscaiado <marcuscaiado@msn.com>
2022-07-17 14:20:28 -0300

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon4 from "../assets/img/nav-icon4.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
@@ -16,14 +16,8 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#">
-                <img src={navIcon1} alt="Icon" />
-              </a>
-              <a href="#">
-                <img src={navIcon2} alt="Icon" />
-              </a>
-              <a href="#">
-                <img src={navIcon3} alt="Icon" />
+              <a href="https://github.com/marcuscaiado">
+                <img src={navIcon4} alt="Icon" />
               </a>
             </div>
             <p>Copyright Marcus Caiado 2022. All Rights Reserved</p>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon4 from "../assets/img/nav-icon4.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
@@ -68,14 +68,8 @@ export const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="#">
-                <img src={navIcon1} alt="" />
-              </a>
-              <a href="#">
-                <img src={navIcon2} alt="" />
-              </a>
-              <a href="#">
-                <img src={navIcon3} alt="" />
+              <a href="https://github.com/marcuscaiado">
+                <img src={navIcon4} alt="" />
               </a>
             </div>
             <button className="vvd" onClick={() => console.log("connect")}>
